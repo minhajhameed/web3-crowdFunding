2f98894d5934cf3ab6ec53e7914a8bb7b264d279
MinhajHameed <minhajham@yahoo.com>
2023-12-18 10:29:06 +0530

    testnet: change sepolia to goerli

--- web3/contracts/CrowdFund.sol
+++ web3/contracts/CrowdFund.sol
@@ -57,7 +57,7 @@ contract CrowdFunding {
         return (campaigns[_id].donators, campaigns[_id].donations);
     }
 
-    function getCampaign() public view returns (Campaign[] memory) {
+    function getCampaigns() public view returns (Campaign[] memory) {
         Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
 
         for(uint i = 0; i < numberOfCampaigns; i++) {
