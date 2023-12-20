50f5373b3e89862152b8a1a73f8771a76ea3c4ab
MinhajHameed <minhajham@yahoo.com>
2023-12-20 10:44:22 +0530

    sidebar links

---
+++ client/src/constants/index.js
@@ -0,0 +1,37 @@
+import { createCampaign, dashboard, logout, payment, profile, withdraw } from '../assets';
+
+export const navlinks = [
+  {
+    name: 'dashboard',
+    imgUrl: dashboard,
+    link: '/',
+  },
+  {
+    name: 'campaign',
+    imgUrl: createCampaign,
+    link: '/create-campaign',
+  },
+  {
+    name: 'collection',
+    imgUrl: payment,
+    link: '/view-all',
+  },
+  {
+    name: 'withdraw',
+    imgUrl: withdraw,
+    link: '/',
+    disabled: true,
+  },
+  {
+    name: 'profile',
+    imgUrl: profile,
+    link: '/profile',
+    disabled: true,
+  },
+  {
+    name: 'logout',
+    imgUrl: logout,
+    link: '/',
+    disabled: true,
+  },
+];
